60c1629706a8a28f9e8e00d2d6f6c273333eb844
Taras Hamkalo <hamkalo.taras@gmail.com>
2026-03-13 17:14:27 +0100

    chore(hw2): extract stages formatter

---
+++ hw2/pkg/logging/stages_formatter.go
@@ -0,0 +1,126 @@
+package logging
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+)
+
+type stageFunc func(record *LogRecord, output *bytes.Buffer)
+
+type StagesFormatterBuilder struct {
+	stages []stageFunc
+}
+
+type StagesFormatter struct {
+	stages []stageFunc
+}
+
+func NewStagesFormatterBuilder() *StagesFormatterBuilder {
+	return &StagesFormatterBuilder{
+		stages: make([]stageFunc, 0, 4),
+	}
+}
+
+func NewDefaultStagesFormatter() *StagesFormatter {
+	return NewStagesFormatterBuilder().
+		WithTimestamp(time.TimeOnly, MAGENTA).
+		WithLogLevel(LogLevelColorMap).
+		WithMessage(NO_COLOR).
+		WithProperties(CYAN, GREEN).
+		Build()
+}
+
+func (s *StagesFormatterBuilder) WithTimestamp(
+	layout string,
+	color AnsiColor,
+) *StagesFormatterBuilder {
+
+	s.stages = append(s.stages, func(record *LogRecord, output *bytes.Buffer) {
+		timestamp := record.Timestamp.Format(layout)
+
+		output.WriteString(string(color))
+		output.WriteString(timestamp)
+		output.WriteString(string(RESET))
+		output.WriteByte(' ')
+	})
+
+	return s
+}
+
+func (s *StagesFormatterBuilder) WithLogLevel(
+	colorMap map[LogLevel]AnsiColor,
+) *StagesFormatterBuilder {
+	s.stages = append(s.stages, func(record *LogRecord, output *bytes.Buffer) {
+		color, ok := colorMap[record.Level]
+		if !ok {
+			color = LogLevelColorMap[record.Level]
+		}
+
+		output.WriteString(string(color))
+		output.WriteString(record.Level.String())
+		output.WriteString(string(RESET))
+		output.WriteByte(' ')
+	})
+
+	return s
+}
+
+func (s *StagesFormatterBuilder) WithMessage(
+	color AnsiColor,
+) *StagesFormatterBuilder {
+	s.stages = append(s.stages, func(record *LogRecord, output *bytes.Buffer) {
+		output.WriteString(string(color))
+		output.WriteString(record.Message)
+		output.WriteString(string(RESET))
+		output.WriteByte(' ')
+	})
+
+	return s
+}
+
+func (s *StagesFormatterBuilder) WithProperties(
+	keyColor AnsiColor,
+	valueColor AnsiColor,
+) *StagesFormatterBuilder {
+
+	s.stages = append(s.stages, func(record *LogRecord, output *bytes.Buffer) {
+		output.WriteString("\t\t")
+		propertiesCount := len(record.Properties) / 2
+		for i := 0; i < propertiesCount; i++ {
+
+			key := record.Properties[i*2]
+			value := record.Properties[i*2+1]
+
+			output.WriteString(string(keyColor))
+			output.WriteString(fmt.Sprint(key))
+			output.WriteString(string(RESET))
+
+			output.WriteByte('=')
+
+			output.WriteString(string(valueColor))
+			output.WriteString(fmt.Sprint(value))
+			output.WriteString(string(RESET))
+
+			output.WriteByte(' ')
+		}
+	})
+
+	return s
+}
+
+func (s *StagesFormatterBuilder) Build() *StagesFormatter {
+	return &StagesFormatter{
+		stages: append([]stageFunc{}, s.stages...),
+	}
+}
+
+func (s *StagesFormatter) Format(record *LogRecord) []byte {
+	var output bytes.Buffer
+	output.Grow(len(record.Message) + 32) // at least timestamp
+	for _, stage := range s.stages {
+		stage(record, &output)
+	}
+
+	return output.Bytes()
+}
