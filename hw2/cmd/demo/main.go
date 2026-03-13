3ba866a3e893138f2f9e54a60f69c5ffdfa4570d
Taras Hamkalo <hamkalo.taras@gmail.com>
2026-03-13 18:16:26 +0100

    chore(hw2): update demo

--- hw2/cmd/demo/main.go
+++ hw2/cmd/demo/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
 	. "hw2/pkg/logging"
+	"os"
 )
 
 func main() {
+	fileAppender, err := NewFileAppender("logs/app-log.jsonl")
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(fileAppender *FileAppender) {
+		err = fileAppender.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}(fileAppender)
+
 	log := NewBaseLogger(
 		[]Sink{
 			NewBaseSink(
@@ -14,15 +28,16 @@ func main() {
 			),
 			NewBaseSink(
 				NewJsonFormatter(),
-				NewConsoleAppender(),
+				fileAppender,
 				LevelDebug,
 			),
 		},
+		os.Stderr,
 	)
 
-	log.Debug("First message", "version", "1.0.0", "tag", "initial")
-	log.Info("First message", "version", "1.0.0", "tag", "initial")
-	log.Warning("First message", "version", "1.0.0", "tag", "initial")
-	log.Error("First message", "version", "1.0.0", "tag", "initial")
+	log.Debug("Debug message", "version", "1.0.0", "tag", os.Stderr)
+	log.Info("Info message", "version", "1.0.0", "tag", "initial")
+	log.Warning("Warning message", "version", "1.0.0", "tag", "initial")
+	log.Error("Error message", "version", "1.0.0", "tag", "initial")
 
 }
