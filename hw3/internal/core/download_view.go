154f99b2ca0a0baa9147438ab7de825979930100
Taras Hamkalo <hamkalo.taras@gmail.com>
2026-04-02 20:35:50 +0200

    chore(hw3): add toString

--- hw3/internal/core/download_view.go
+++ hw3/internal/core/download_view.go
@@ -1,6 +1,9 @@
 package core
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type DownloadView struct {
 	Id string
@@ -19,4 +22,22 @@ type DownloadView struct {
 	EndTime       time.Time
 
 	Cause error
+}
+
+// generated for demo
+func (d DownloadView) String() string {
+	return fmt.Sprintf(
+		"Id=%s\nUrl=%s\nDestination=%s\nStatus=%v\nBytesDownloaded=%d\nExpectedSize=%d\nTotalSize=%d\nRequestedTime=%s\nStartTime=%s\nEndTime=%s\nCause=%v",
+		d.Id,
+		d.Url,
+		d.Destination,
+		d.Status,
+		d.BytesDownloaded,
+		d.ExpectedSize,
+		d.TotalSize,
+		d.RequestedTime,
+		d.StartTime,
+		d.EndTime,
+		d.Cause,
+	)
 }
