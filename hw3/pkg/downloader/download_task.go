2be16d6d7b3efaff407a80e99a15cb0aaa156fbf
Taras Hamkalo <hamkalo.taras@gmail.com>
2026-04-04 12:37:24 +0200

    docs(hw3): add comments

--- hw3/pkg/downloader/download_task.go
+++ hw3/pkg/downloader/download_task.go
@@ -15,25 +15,39 @@ import (
 )
 
 // DownloadTask represents a temporary object of running download.
-// Based on that, it does not contain status but rather holds data
-// needed to cancel download or track its completion.
+// It stores metadata needed to properly process IO operations and
+// trigger updates of DownloadRecord (downloads' metadata).
+// Given task does not modify DonwloadRecord entry directly, but initiates
+// events (handled in Downloader).
 // For full metadata stored about donwload see DownloadRecord.
 type DownloadTask struct {
+
+	// id is task id (default UUID)
 	id string
 
-	// cross reference
+	// downloadId is DownloadRecord associated with given task
 	downloadId string
 
+	// url is http resource being downloaded
 	url string
 
+	// destination is path on local file system, where resource gonna be stored
+	// in case of partial completion (IO error/task cancellation) resource won't
+	// be written to disk/removed.
+	// -- if everything goes well =)
 	destination string
 
-	// will be closed after task finishes
+	// errorExited is used to indicate whether task error occurred and partially
+	// written resource should be removed from FS
 	errorExited atomic.Bool
 
+	// progress is instance of ProgressWriter tracking this download process
+	// used to query status updates (in separate routine) and send update events
+	// to downloader
 	progress *ProgressWriter
 }
 
+// NewDownloadTask create default DownloadTask with ID set to UUID
 func NewDownloadTask(
 	downloadId string,
 	url string,
@@ -46,6 +60,8 @@ func NewDownloadTask(
 		url:         url,
 		destination: destination,
 
+		errorExited: atomic.Bool{}, // initial false
+
 		progress: NewProgressWriter(),
 	}
 }
@@ -54,19 +70,26 @@ func (d *DownloadTask) Id() string {
 	return d.id
 }
 
+// Execute handles IO operations associated with HTTP resource download process.
+// Context passed to it, is used to cancel both HTTP headers request and
+// body download/write process.
+// NOTE: no further timeouts are set over HTTP request as body download process
+// can last nondeterministically long.
 func (d *DownloadTask) Execute(ctx context.Context, downloader *Downloader) {
 	file, err := d.openFile(downloader)
 	if err != nil {
+		// file was not created
 		d.errorExited.Store(true)
 		return
 	}
 
+	// register function to close file and in case error occurred, remove it
+	// NOTE: other way would be to write temporary file and then local IO copy
 	defer (func() {
 		d.closeFile(file, downloader)
 		if !d.errorExited.Load() {
 			return
 		}
-
 		if err := os.Remove(file.Name()); err != nil {
 			downloader.logger.Warn(
 				"error removing partially written file", zap.Error(err),
@@ -74,6 +97,7 @@ func (d *DownloadTask) Execute(ctx context.Context, downloader *Downloader) {
 		}
 	})()
 
+	// No further timeouts over context as it would include body download as well
 	resp, err := d.doRequest(ctx, downloader)
 	if err != nil {
 		d.errorExited.Store(true)
@@ -95,6 +119,8 @@ func (d *DownloadTask) Execute(ctx context.Context, downloader *Downloader) {
 	d.finishDownload(downloader)
 }
 
+// openFile opens file at destination, in case exists error occur
+// Given error creates event sent to downloader
 func (d *DownloadTask) openFile(downloader *Downloader) (*os.File, error) {
 	file, err := os.OpenFile(
 		d.destination,
@@ -116,11 +142,13 @@ func (d *DownloadTask) closeFile(file *os.File, downloader *Downloader) {
 	}
 }
 
+// doRequest makes initial get request to url, errors which occur
+// are sent to downloader via events channel, in case context cancellation
+// occur, cancel event sent instead of error.
 func (d *DownloadTask) doRequest(
 	ctx context.Context,
 	downloader *Downloader,
 ) (*http.Response, error) {
-	// No further timeouts over context as it would include body download as well
 	req, err := http.NewRequestWithContext(ctx, "GET", d.url, nil)
 	if err != nil {
 		downloader.writeEventsChan() <- NewDownloadError(
@@ -157,6 +185,8 @@ func (d *DownloadTask) closeResponse(
 	}
 }
 
+// validateResponse verify response status and sent event to downloader in case
+// of error
 func (d *DownloadTask) validateResponse(
 	resp *http.Response,
 	downloader *Downloader,
@@ -169,6 +199,8 @@ func (d *DownloadTask) validateResponse(
 	return nil
 }
 
+// startDownload notifies downloader about download start, occurs
+// right before download body starts.
 func (d *DownloadTask) startDownload(
 	downloader *Downloader,
 	resp *http.Response,
@@ -177,6 +209,10 @@ func (d *DownloadTask) startDownload(
 	d.progress.Reset()
 }
 
+// downloadBody handles IO (download and pipe) associated with body download.
+// Registers a routine polling for amount of read bytes from progressWriter.
+// In case of error produces DownloadErrorEvent, in case of ctx cancellation
+// DownloadCancelEvent.
 func (d *DownloadTask) downloadBody(
 	ctx context.Context,
 	file *os.File,
@@ -204,6 +240,8 @@ func (d *DownloadTask) downloadBody(
 	return nil
 }
 
+// tickProgress registers tickers with interval provided by downloader,
+// and sends DownloadUpdateEvents till ctx is not canceled.
 func (d *DownloadTask) tickProgress(
 	ctx context.Context,
 	downloader *Downloader,
@@ -222,6 +260,8 @@ func (d *DownloadTask) tickProgress(
 	}
 }
 
+// finishDownload indicates proper completion of download, sending event
+// to downloader.
 func (d *DownloadTask) finishDownload(downloader *Downloader) {
 	downloader.writeEventsChan() <- NewDownloadComplete(
 		d, d.progress.BytesRead(),
