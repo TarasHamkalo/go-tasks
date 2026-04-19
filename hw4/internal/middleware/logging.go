d1571690de89ddaaf8ba4c227ed04d8dbb7a3f0b
Taras Hamkalo <hamkalo.taras@gmail.com>
2026-04-19 18:20:15 +0200

    refactor(hw3): add grpc version of middleware

--- hw4/internal/middleware/logging.go
+++ hw4/internal/middleware/logging.go
@@ -1,12 +1,14 @@
 package middleware
 
 import (
+	"context"
 	"net/http"
 
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
 )
 
-func Logging(logger *zap.Logger, next http.Handler) http.Handler {
+func HttpLogging(logger *zap.Logger, next http.Handler) http.Handler {
 	f := func(w http.ResponseWriter, r *http.Request) {
 		logger.Info(
 			"received request",
@@ -19,4 +21,27 @@ func Logging(logger *zap.Logger, next http.Handler) http.Handler {
 	}
 
 	return http.HandlerFunc(f)
+}
+
+func GrpcLogging(logger *zap.Logger) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req any,
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (any, error) {
+		logger.Info(
+			"received request",
+			zap.String("trace", GetTraceId(ctx)),
+			zap.String("procedure", info.FullMethod),
+		)
+		res, err := handler(ctx, req)
+
+		logger.Info(
+			"request handled",
+			zap.String("trace", GetTraceId(ctx)),
+			zap.Error(err),
+		)
+		return res, err
+	}
 }
--- hw4/internal/middleware/tracing.go
+++ hw4/internal/middleware/tracing.go
@@ -5,13 +5,14 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
+	"google.golang.org/grpc"
 )
 
 type contextKey string
 
 const traceIdKey contextKey = "traceId"
 
-func Tracing(next http.Handler) http.Handler {
+func HttpTracing(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		id := uuid.New().String()[:8]
 		ctx := context.WithValue(r.Context(), traceIdKey, id)
@@ -23,4 +24,18 @@ func GetTraceId(ctx context.Context) string {
 	id, _ := ctx.Value(traceIdKey).(string)
 	// empty string :)
 	return id
+}
+
+func GrpcTracing() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req any,
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (any, error) {
+
+		id := uuid.New().String()[:8]
+		ctx = context.WithValue(ctx, traceIdKey, id)
+		return handler(ctx, req)
+	}
 }
