9819cd18951567fb0c502ad0e6b6f089e15d9ecd
Taras Hamkalo <hamkalo.taras@gmail.com>
2026-04-20 11:44:12 +0200

    docs(hw4): extend examples

--- hw4/requests/demo.sh
+++ hw4/requests/demo.sh
@@ -4,6 +4,18 @@ GRPC_ADDR="localhost:8081"
 HTTP_ADDR="http://localhost:8080"
 SERVICE="ManagementService"
 
+log_http() {
+  local method=$1
+  local path=$2
+  local body=$3
+
+  if [ -z "$body" ]; then
+    echo ">> HTTP REQUEST: $method $path"
+  else
+    echo ">> HTTP REQUEST: $method $path body=$body"
+  fi
+}
+
 echo "== Clear database =="
 grpcurl -insecure -d '{}' $GRPC_ADDR $SERVICE/ClearDatabase
 
@@ -16,22 +28,26 @@ BODY='["user-1","user-2"]'
 B64_BODY=$(echo -n "$BODY" | base64)
 
 grpcurl -insecure \
-  -d '{
-    "method": "GET",
-    "path": "/users",
-    "status_code": 200,
-    "response_body": '\"$B64_BODY\"'
-  }' \
+  -d "{
+    \"method\": \"GET\",
+    \"path\": \"/users\",
+    \"status_code\": 200,
+    \"response_body\": \"$B64_BODY\"
+  }" \
   $GRPC_ADDR \
   $SERVICE/SetReply
 
 echo "-- Dump database --"
 grpcurl -insecure $GRPC_ADDR $SERVICE/DumpDatabase
+echo -e "\n"
 
-echo "-- HTTP call /users --"
+echo "-- HTTP call /users (should match, 200) --"
+log_http "GET" "/users"
 curl -i $HTTP_ADDR/users
+echo -e "\n"
 
-echo "-- HTTP call to /invalid --"
+echo "-- HTTP call /invalid (path not configured, 404) --"
+log_http "GET" "/invalid"
 curl -i $HTTP_ADDR/invalid
 
 echo -e "\n"
@@ -54,13 +70,20 @@ grpcurl -insecure \
   $GRPC_ADDR \
   $SERVICE/SetReply
 
-echo "-- HTTP call (match) --"
+echo "-- Dump database --"
+grpcurl -insecure $GRPC_ADDR $SERVICE/DumpDatabase
+echo -e "\n"
+echo "-- HTTP call (req body match, 200) --"
+log_http "POST" "/users/user-1" "$REQ_BODY"
 curl -i -X POST $HTTP_ADDR/users/user-1 -d "$REQ_BODY"
 echo -e "\n"
 
-echo "-- HTTP call (mismatch → 404) --"
-curl -i -X POST $HTTP_ADDR/users/user-1 -d '{"name":"wrong"}'
+echo "-- HTTP call (req body mismatch, 404) --"
+BAD_BODY='{"name":"wrong"}'
+log_http "POST" "/users/user-1" "$BAD_BODY"
+curl -i -X POST $HTTP_ADDR/users/user-1 -d "$BAD_BODY"
 echo -e "\n"
+
 ########################################
 # Example 3: Query order ignored
 ########################################
@@ -79,8 +102,24 @@ grpcurl -insecure \
   $GRPC_ADDR \
   $SERVICE/SetReply
 
-echo "-- HTTP call (reordered query) --"
-curl -i "$HTTP_ADDR/test?name=Surname&name=Nickname&name=Name"
+echo "-- Dump database --"
+grpcurl -insecure $GRPC_ADDR $SERVICE/DumpDatabase
+echo -e "\n"
+
+QUERY="/test?name=Surname&name=Nickname&name=Name"
+echo "-- HTTP call (reordered query, match, 200) --"
+
+echo -e "Registered: /test?name=Name&name=Surname&name=Nickname",
+echo -e "\n"
+
+log_http "GET" "$QUERY"
+curl -i "$HTTP_ADDR$QUERY"
+echo -e "\n"
+
+QUERY="/test?name=NotName&name=Nickname&name=Name"
+echo "-- HTTP call (name=NotName, mismatch, 404) --"
+log_http "GET" "$QUERY"
+curl -i "$HTTP_ADDR$QUERY"
 echo -e "\n"
 
 ########################################
@@ -101,12 +140,18 @@ grpcurl -insecure \
   $GRPC_ADDR \
   $SERVICE/SetReply
 
-echo "-- HTTP call (bbb → should be 404) --"
+echo "-- Dump database --"
+grpcurl -insecure $GRPC_ADDR $SERVICE/DumpDatabase
+echo -e "\n"
+
+echo "-- HTTP call (body mismatch, 404) --"
+log_http "POST" "/test" "bbb"
 curl -i -X POST $HTTP_ADDR/test -d "bbb"
 echo -e "\n"
 
+
 ########################################
-# Debug helpers
+# GRPC endpoints
 ########################################
 echo "== DumpDatabase =="
 grpcurl -insecure -d '{}' $GRPC_ADDR $SERVICE/DumpDatabase
