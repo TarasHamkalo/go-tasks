751a510bddd894f6d0ac8dfed95df81d4477d907
Taras Hamkalo <hamkalo.taras@gmail.com>
2026-05-15 11:36:50 +0200

    feat: add initial queries/repository impl

--- hw5/internal/messaging/repository.go
+++ hw5/internal/messaging/repository.go
@@ -10,7 +10,7 @@ type Repository interface {
 	Close() error
 
 	// Chats
-	CreateChat(ctx context.Context, chat Chat) error
+	InsertChat(ctx context.Context, chat Chat) error
 	GetUserChats(ctx context.Context, userId string) ([]Chat, error)
 
 	// Chat members
--- hw5/internal/messaging/sqlite_repository.go
+++ hw5/internal/messaging/sqlite_repository.go
@@ -1,12 +1,11 @@
 package messaging
 
 import (
-	// "context"
-	// "strings"
-	// "time"
-	//
-	// "github.com/jmoiron/sqlx"
-	// _ "modernc.org/sqlite"
+	"context"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "modernc.org/sqlite"
 )
 
 const (
@@ -21,12 +20,13 @@ const (
 					is_group INTEGER NOT NULL
 			);
 
-	CREATE TABLE IF NOT EXISTS chat_members (
+	CREATE TABLE IF NOT EXISTS 
+		chat_members(
 			chat_id TEXT NOT NULL,
 			user_id TEXT NOT NULL, -- FK to profile service users
 			PRIMARY KEY (chat_id, user_id),
 			FOREIGN KEY (chat_id) REFERENCES chats(id) ON DELETE CASCADE
-	);
+		);
 
 	CREATE TABLE IF NOT EXISTS 
 		messages(
@@ -55,72 +55,312 @@ const (
 		);
 	`
 
-	INSERT_QUERY = `
-		INSERT INTO profiles (user_id, username, password) 
-		VALUES (:user_id, :username, :password)
+	// --- Chats ---
+	INSERT_CHAT_QUERY = `
+		INSERT INTO chats (id, name, is_group) 
+		VALUES (:id, :name, :is_group)
 	`
 
-	GET_MESSAGE_BY_ID_QUERY = `
-		SELECT user_id, username, password FROM profiles 
-		WHERE user_id = ?
+	GET_CHATS_BY_USER_QUERY = `
+		SELECT c.id, c.name, c.is_group 
+		FROM chats c
+		INNER JOIN chat_members cm ON c.id = cm.chat_id
+		WHERE cm.user_id = ?
+	`
+
+	// --- Chat Members ---
+	ADD_CHAT_MEMBER_QUERY = `
+		INSERT INTO chat_members (chat_id, user_id) 
+		VALUES (:chat_id, :user_id)
+		ON CONFLICT(chat_id, user_id) DO NOTHING
+	`
+
+	REMOVE_CHAT_MEMBER_QUERY = `
+		DELETE FROM chat_members 
+		WHERE chat_id = :chat_id AND user_id = :user_id 
+	`
+
+	GET_CHAT_MEMBERS_QUERY = `
+		SELECT user_id 
+		FROM chat_members 
+		WHERE chat_id = ?
+	`
+
+	// --- Messages ---
+	INSERT_MESSAGE_QUERY = `
+		INSERT INTO messages (id, chat_id, sender_id, content, sent_at) 
+		VALUES (:id, :chat_id, :sender_id, :content, :sent_at)
+	`
+
+	// --- Tracking and Delivery ---
+	// Note: Used for pulling messages for offline users.
+	// Messages are sort by sent_at so the client app appends them in
+	// historical order.
+	INSERT_MESSAGE_ACK_QUERY = `
+		INSERT INTO message_acks (message_id, user_id, delivered_at, read_at)
+		VALUES (:message_id, :user_id, :delivered_at, :read_at)
+	`
+
+	GET_UNDELIVERED_MESSAGES_QUERY = `
+		SELECT m.id, m.chat_id, m.sender_id, m.content, m.sent_at 
+		FROM messages m
+		INNER JOIN message_acks ma ON m.id = ma.message_id
+		WHERE ma.user_id = ? AND ma.delivered_at IS NULL
+		ORDER BY m.sent_at ASC
+	`
+
+	SET_MESSAGE_DELIVERED_QUERY = `
+		UPDATE message_acks 
+		SET delivered_at = ? 
+		WHERE message_id = ? AND user_id = ? AND delivered_at IS NULL
+	`
+
+	SET_MESSAGE_READ_QUERY = `
+		UPDATE message_acks 
+		SET read_at = ? 
+		WHERE message_id = ? AND user_id = ? AND read_at IS NULL
+	`
+
+	GET_MESSAGE_ACKS_QUERY = `
+		SELECT message_id, user_id, delivered_at, read_at 
+		FROM message_acks 
+		WHERE message_id = ?
 	`
 )
 
-// type SqliteRepository struct {
-// 	Db *sqlx.DB
-// }
-//
-// func NewSqliteRepository(dbPath string) (*SqliteRepository, error) {
-// 	db, err := sqlx.Open("sqlite", dbPath)
-// 	if err != nil {
-// 		return &SqliteRepository{}, err
-// 	}
-// 	return &SqliteRepository{Db: db}, nil
-// }
-//
-// func (r SqliteRepository) InitializeSchema(ctx context.Context) error {
-// 	queryCtx, cancel := context.WithTimeout(
-// 		ctx, time.Duration(time.Second*5),
-// 	)
-//
-// 	defer cancel()
-// 	_, err := r.Db.ExecContext(queryCtx, SCHEMA_QUERY)
-//
-// 	return err
-// }
-//
-// func (r SqliteRepository) InsertProfile(ctx context.Context, p Profile) error {
-// 	queryCtx, cancel := context.WithTimeout(
-// 		ctx, time.Duration(time.Second*2),
-// 	)
-// 	defer cancel()
-// 	_, err := r.Db.NamedExecContext(queryCtx, INSERT_QUERY, p)
-// 	if err != nil && isUniqueConstraint(err) {
-// 		return ErrorUniqueConstraintViolated
-// 	}
-//
-// 	return err
-// }
-//
-// func (r SqliteRepository) GetProfileByUserId(
-// 	ctx context.Context, userId string,
-// ) (Profile, error) {
-// 	queryCtx, cancel := context.WithTimeout(
-// 		ctx, time.Duration(time.Second*2),
-// 	)
-// 	defer cancel()
-// 	p := Profile{}
-// 	err := r.Db.GetContext(queryCtx, &p, GET_BY_USER_ID_QUERY, userId)
-// 	return p, err
-// }
-//
-// func (r SqliteRepository) Close() error {
-// 	return r.Db.Close()
-// }
-//
-// // isUniqueConstraint verifies whether given SQL error is unique constraint
-// // violation. Pretty hard to check with given API of modernc.org/sqlite
-// func isUniqueConstraint(err error) bool {
-// 	return err != nil &&
-// 		strings.Contains(err.Error(), "UNIQUE constraint failed")
-// }
+type SqliteRepository struct {
+	Db *sqlx.DB
+}
+
+func NewSqliteRepository(dbPath string) (*SqliteRepository, error) {
+	db, err := sqlx.Open("sqlite", dbPath)
+	if err != nil {
+		return &SqliteRepository{}, err
+	}
+	return &SqliteRepository{Db: db}, nil
+}
+
+func (r SqliteRepository) InitializeSchema(ctx context.Context) error {
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*5),
+	)
+
+	defer cancel()
+	_, err := r.Db.ExecContext(queryCtx, SCHEMA_QUERY)
+
+	return err
+}
+
+func (r SqliteRepository) Close() error {
+	return r.Db.Close()
+}
+
+func (r SqliteRepository) InsertChat(ctx context.Context, chat Chat) error {
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*2),
+	)
+	defer cancel()
+	_, err := r.Db.NamedExecContext(queryCtx, INSERT_CHAT_QUERY, chat)
+	return err
+}
+
+func (r SqliteRepository) GetUserChats(
+	ctx context.Context, userId string,
+) ([]Chat, error) {
+	chats := []Chat{}
+
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*2),
+	)
+	defer cancel()
+
+	err := r.Db.SelectContext(queryCtx, &chats, GET_CHATS_BY_USER_QUERY, userId)
+	return chats, err
+}
+
+func (r SqliteRepository) AddChatMember(
+	ctx context.Context, chatId, userId string,
+) error {
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*2),
+	)
+	defer cancel()
+
+	_, err := r.Db.NamedExecContext(
+		queryCtx,
+		ADD_CHAT_MEMBER_QUERY,
+		map[string]interface{}{
+			"chat_id": chatId,
+			"user_id": userId,
+		},
+	)
+	return err
+}
+
+func (r SqliteRepository) RemoveChatMember(
+	ctx context.Context, chatId, userId string,
+) error {
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*2),
+	)
+	defer cancel()
+
+	_, err := r.Db.NamedExecContext(
+		queryCtx,
+		REMOVE_CHAT_MEMBER_QUERY,
+		map[string]interface{}{
+			"chat_id": chatId,
+			"user_id": userId,
+		},
+	)
+	return err
+}
+
+func (r SqliteRepository) GetChatMembers(
+	ctx context.Context, chatId string,
+) ([]string, error) {
+	ids := []string{}
+
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*2),
+	)
+	defer cancel()
+
+	err := r.Db.SelectContext(
+		queryCtx,
+		&ids,
+		GET_CHAT_MEMBERS_QUERY,
+		chatId,
+	)
+
+	return ids, err
+}
+
+func (r SqliteRepository) InsertMessage(
+	ctx context.Context, message Message,
+) error {
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*2),
+	)
+	defer cancel()
+	_, err := r.Db.NamedExecContext(queryCtx, INSERT_MESSAGE_QUERY, message)
+	return err
+}
+
+// TODO: message and acks could be inserted in a single transaction for consistency
+// client should buffer message until success write
+func (r SqliteRepository) InsertMessageAcks(
+	ctx context.Context, acks []MessageAck,
+) error {
+	if len(acks) == 0 {
+		return nil
+	}
+
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*5), // larger timeout
+	)
+	defer cancel()
+
+	tx, err := r.Db.BeginTxx(queryCtx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	_, err = tx.NamedExecContext(
+		queryCtx,
+		INSERT_MESSAGE_ACK_QUERY,
+		acks,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func (r SqliteRepository) GetUndeliveredMessages(
+	ctx context.Context, userId string,
+) ([]Message, error) {
+	messages := []Message{}
+
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*2),
+	)
+	defer cancel()
+
+	err := r.Db.SelectContext(
+		queryCtx,
+		&messages,
+		GET_UNDELIVERED_MESSAGES_QUERY,
+		userId,
+	)
+
+	return messages, err
+}
+
+func (r SqliteRepository) SetMessageDelivered(
+	ctx context.Context,
+	messageId string,
+	userId string,
+	deliveredAt time.Time,
+) error {
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*2),
+	)
+	defer cancel()
+
+	_, err := r.Db.ExecContext(
+		queryCtx,
+		SET_MESSAGE_DELIVERED_QUERY,
+		deliveredAt,
+		messageId,
+		userId,
+	)
+
+	return err
+}
+
+func (r SqliteRepository) SetMessageRead(
+	ctx context.Context,
+	messageId string,
+	userId string,
+	readAt time.Time,
+) error {
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*2),
+	)
+	defer cancel()
+
+	_, err := r.Db.ExecContext(
+		queryCtx,
+		SET_MESSAGE_READ_QUERY,
+		readAt,
+		messageId,
+		userId,
+	)
+
+	return err
+}
+
+func (r SqliteRepository) GetMessageAcks(
+	ctx context.Context,
+	messageId string,
+) ([]MessageAck, error) {
+	acks := []MessageAck{}
+
+	queryCtx, cancel := context.WithTimeout(
+		ctx, time.Duration(time.Second*2),
+	)
+	defer cancel()
+
+	err := r.Db.SelectContext(
+		queryCtx,
+		&acks,
+		GET_MESSAGE_ACKS_QUERY,
+		messageId,
+	)
+
+	return acks, err
+}
